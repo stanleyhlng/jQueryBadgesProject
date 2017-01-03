46e20b53efc05016cb5590929bed61c0e7136e1d
stanleyhlng <stanleyhlng@yahoo.com.hk>
2017-01-02 16:49:44 -0800

    Display the courses I have completed on Code School by pulling data from the Code School website

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,39 @@
 $(function() {
+    // Make an Ajax request to the "Report Card URL"
+    $.ajax({
+        url: 'https://www.codeschool.com/users/stanleyhlng.json',
+        dataType: 'jsonp',
+        success: function (response) {
+            // handle response
+            // console.log(response);
+            var $badges = $('#badges');
+            response.courses.completed.forEach(function eachCource(course) {
 
-  // your code will go here
+                // An Element per Completed Course
+                var $course = $('<div></div>')
+                    .addClass('course');
+                $badges.append($course);
 
+                // Adding Course Titles
+                var $title = $('<h3/>')
+                    .append(course.title);
+                $course.append($title);
+
+                // Adding Course Images
+                var $badge = $('<img/>')
+                    .attr('src', course.badge);
+                $course.append($badge);
+
+                // Adding Course Buttons
+                var $link = $('<a />')
+                    .addClass('btn btn-primary')
+                    .attr('target', '_blank')
+                    .attr('href', course.url)
+                    .append('See Course');
+                $course.append($link);
+
+                // console.log($('<div />').append($course.clone()).html());
+            });
+        }
+    });
 });
